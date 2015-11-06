0f55febbcf55043c454a8719d3ced255c96cb049
Luke Harper <lukeharper@live.ca>
2015-11-06 13:32:33 -0500

    World base setup
    constructor setup

--- Header Files/world.h
+++ Header Files/world.h
@@ -1,1 +1,23 @@
-//world.h
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "ogre_application.h"
+
+class World
+{
+public:
+	World(Ogre::SceneNode* rootNode); //only creates a new sceneNode for the World, please use init to setup the world
+	~World();
+
+	/*	initWorld()
+	*	Initializes the world to default.
+	*/
+	void initWorld();
+
+private:
+	//things in the world go here.
+	Ogre::SceneNode* worldSceneNode;
+
+};
+
+#endif
--- Source Files/world.cpp
+++ Source Files/world.cpp
@@ -1,1 +1,19 @@
 //world.cpp
+
+#include "world.h"
+
+World::World(Ogre::SceneNode* rootNode/*, Camera* cam, InputManager* inMan*/): 
+	worldSceneNode(rootNode->createChildSceneNode("_worldSceneNode_"))/*, camera(cam), inputManager(inMan) */ //note underscores so someone decided to create earth and label it worldSceneNode, wont have conflicts
+{
+	//lattahdah lattahdah
+}
+
+World::~World()
+{
+	//TODO memory cleanup
+}
+
+void World::initWorld()
+{
+	//TODO setup stuff for the world goes here.
+}
