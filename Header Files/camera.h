e34fcfc231583978c4a416a64cd8d82f803c2a29
Luke Harper <lukeharper@live.ca>
2015-11-06 13:45:50 -0500

    Added base setup for camera stuff
    still needed to move the camera stuff over to here

--- Header Files/camera.h
+++ Header Files/camera.h
@@ -1,1 +1,30 @@
-//camera.h
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "ogre_application.h"
+
+#define CAMERA_INITIAL_DISTANCE 2.0
+class Camera
+{
+public:
+	Camera(Ogre::Camera* cam);
+	~Camera();
+
+	/*	attachTo
+	*	in:			SceneNode you wish to attach
+	*	Purpose:	To attach the camera to a Node, this will be attached starting  at a certain distance away (can be specified with the setDistance function)
+	*/
+	void attachTo(Ogre::SceneNode* followNode);
+
+	/*	setDistance
+	*	in:			float, the distance you wish to be behind the attached node, can be negative
+	*	Purpose:	to set the distnace away from a node you wish the camera to be (0 for first person, negative to turn camera around)
+	*/
+	void setDistance(float distanceFromNode);
+
+private:
+	Ogre::Camera* camera;
+	float distance;
+
+};
+#endif
--- Source Files/camera.cpp
+++ Source Files/camera.cpp
@@ -1,1 +1,19 @@
-//camera.cpp
+#include "camera.h"
+
+Camera::Camera(Ogre::Camera* cam): camera(cam), distance(CAMERA_INITIAL_DISTANCE)
+{
+
+}
+Camera::~Camera()
+{
+	//TODO MEMORY MANAGEMENT
+}
+
+void Camera::attachTo(Ogre::SceneNode* followNode)
+{
+	//TODO move the hooking up a camera from the ogre_application to here
+}
+void Camera::setDistance(float distanceFromNode)
+{
+	distance = distanceFromNode;
+}
