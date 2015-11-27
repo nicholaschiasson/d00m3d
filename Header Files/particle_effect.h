0caa7884205e069f6d59aed33e97db858b027c91
Luke Harper <lukeharper@live.ca>
2015-11-27 12:20:47 -0500

    changed name from particle to particle effect
    yeah that

---
+++ Header Files/particle_effect.h
@@ -0,0 +1,8 @@
+#ifndef PARTICLE_EFFECT_H
+#define PARTICLE_EFFECT_H
+
+class ParticleEffect{
+
+}
+
+#endif //PARTICLE_EFFECT_H
---
+++ Header Files/particle_engine.h
@@ -0,0 +1,8 @@
+#ifndef PARTICLE_ENGINE_H
+#define PARTICLE_ENGINE_H
+
+class ParticleEngine{
+
+}
+
+#endif //PARTICLE_ENGINE_H
