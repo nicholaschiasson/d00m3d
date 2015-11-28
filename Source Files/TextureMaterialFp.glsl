8e4c1783f32e52504bae449e58c40dff3616b658
Elephly <elephly88@gmail.com>
2015-11-28 00:42:43 -0500

    Texturing

---
+++ Source Files/TextureMaterialFp.glsl
@@ -0,0 +1,64 @@
+#version 400
+
+// Attributes passed from the vertex shader
+in vec3 position_interp;
+in vec3 normal_interp;
+in vec4 colour_interp;
+in vec2 uv_interp;
+in vec3 light_pos;
+
+//attributes passed in from the material file
+uniform vec4 ambient_colour;
+uniform vec4 diffuse_colour;
+uniform vec4 specular_colour;
+uniform float phong_exponent;
+uniform sampler2D texture_map;
+uniform float custom_colour;
+
+void calculate_light(in vec3 light_pos, inout float Id, inout float Is)
+{
+
+	// Phong shading
+
+	vec3 N, // Interpolated normal for fragment
+	     L, // Light-source direction
+		 V, // View direction
+		 R; // Refelction Vector
+
+	// Compute Lambertian lighting Id
+    N = normalize(normal_interp);
+
+	L = (light_pos - position_interp);
+	L = normalize(L);
+
+	Id += max(dot(N, L), 0.0);
+	
+	// Compute specular term for Phong shading
+	// Initially: V = (eye_position - position_interp);
+	V = - position_interp; // Eye position is (0, 0, 0) in view coordinates
+    V = normalize(V);
+
+    R = -L + 2 *dot(L, N) * N; // Halfway vector
+    R = normalize(R);
+
+    float spec_angle_cos = max(dot(V, R), 0.0);
+	Is += pow(spec_angle_cos, phong_exponent);
+}
+
+void main() 
+{
+	float Id = 0;
+	float Is = 0;
+
+	vec4 light_colour = vec4(0,0,0,0);
+	vec4 black = vec4(0,0,0,1);
+
+	calculate_light(light_pos, Id, Is);
+	
+	vec4 pixel = texture(texture_map, uv_interp);
+
+	light_colour = ambient_colour * pixel + Id * diffuse_colour * pixel + Is * specular_colour;
+	
+	gl_FragColor = light_colour;
+}
+
---
+++ Source Files/TextureMaterialVp.glsl
@@ -0,0 +1,32 @@
+#version 400
+
+// Attributes passed automatically by OGRE
+in vec3 vertex;
+in vec3 normal;
+in vec4 colour;
+in vec2 uv0;
+
+// Attributes passed with the material file
+uniform mat4 world_mat;
+uniform mat4 view_mat;
+uniform mat4 projection_mat;
+uniform mat4 normal_mat;
+uniform vec3 light_position;
+
+// Attributes forwarded to the fragment shader
+out vec4 colour_interp;
+out vec3 position_interp;
+out vec3 normal_interp;
+out vec2 uv_interp;
+out vec3 light_pos;
+
+
+void main()
+{
+    gl_Position = projection_mat * view_mat * world_mat * vec4(vertex, 1.0);
+	position_interp = vec3(view_mat * world_mat * vec4(vertex, 1.0));
+    colour_interp = colour;
+	normal_interp = vec3(normal_mat * vec4(normal, 0.0));
+	uv_interp = uv0;
+    light_pos = vec3(view_mat * vec4(light_position, 1.0));
+}
