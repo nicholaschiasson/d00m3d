8690ab292fa02d9224514012e390326f2b591b6f
Elephly <elephly88@gmail.com>
2015-11-29 12:36:46 -0500

    I fucking love it when my code evaporates

--- Header Files/system_component.h
+++ Header Files/system_component.h
@@ -1,18 +1,28 @@
 #ifndef SYSTEM_COMPONENT_H
 #define SYSTEM_COMPONENT_H
 
+#include <OgreString.h>
+
 class SystemComponent
 {
 public:
 	enum SYSTEM_TYPE
 	{
+		SYSTEM_ARTILLERY,
+		SYSTEM_DEFENSE,
+		SYSTEM_FUEL,
 		SYSTEM_NAVIGATIONAL,
 	};
 
 	SystemComponent();
 	~SystemComponent();
 
+	float getHealth();
+	Ogre::String getName();
+	SYSTEM_TYPE getType();
+
 private:
+	float health;
 	SYSTEM_TYPE type;
 
 };
--- Source Files/system_component.cpp
+++ Source Files/system_component.cpp
@@ -7,3 +7,30 @@ SystemComponent::SystemComponent()
 SystemComponent::~SystemComponent()
 {
 }
+
+float SystemComponent::getHealth()
+{
+	return health;
+}
+
+Ogre::String SystemComponent::getName()
+{
+	switch (type)
+	{
+	case SYSTEM_ARTILLERY:
+		return "Artillery";
+	case SYSTEM_DEFENSE:
+		return "Defense";
+	case SYSTEM_FUEL:
+		return "Fuel";
+	case SYSTEM_NAVIGATIONAL:
+		return "Navigational";
+	default:
+		return "Unknown";
+	}
+}
+
+SystemComponent::SYSTEM_TYPE SystemComponent::getType()
+{
+	return type;
+}
